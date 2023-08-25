688f04acc1d7938860a258d94cfdded56c2dc127
Quang Vinh Dang <dqvinh808@gmail.com>
2023-08-25 09:54:08 -0700

    Commit test

---
+++ cartpole.py
@@ -0,0 +1,62 @@
+import torch
+import gym
+from model import Model
+from collections import deque
+import numpy as np
+
+env = gym.make("CartPole-v1", render_mode = "rgb_array")
+policy = Model(4, 2)
+
+def reinforce(policy, n_training_episodes, max_t, gamma, print_every):
+    scores_deque = deque(maxlen=100)
+    scores = []
+    for i_episode in range(1, n_training_episodes+1):
+        saved_log_probs = []
+        rewards = []
+        state, info = env.reset()
+        # Line 4 of pseudocode
+        for t in range(max_t):
+            # print(state)
+            action, log_prob = policy.act(torch.tensor(state))
+            saved_log_probs.append(log_prob)
+            state, reward, done, terminated, _ = env.step(action)
+            rewards.append(reward)
+            if done or terminated:
+                break 
+        scores_deque.append(sum(rewards))
+        scores.append(sum(rewards))
+        
+        returns = deque(maxlen=max_t) 
+        n_steps = len(rewards) 
+        
+        for t in range(n_steps)[::-1]:
+            disc_return_t = (returns[0] if len(returns)>0 else 0)
+            returns.appendleft( gamma*disc_return_t + rewards[t]   )    
+            
+        eps = np.finfo(np.float32).eps.item()
+        returns = torch.tensor(returns)
+        returns = (returns - returns.mean()) / (returns.std() + eps)
+        
+        policy_loss = []
+        for log_prob, disc_return in zip(saved_log_probs, returns):
+            policy_loss.append(-log_prob * disc_return)
+        
+        # print(policy_loss)
+        policy_loss = torch.cat(policy_loss).sum()
+        
+        policy.optimizer.zero_grad()
+        policy_loss.backward()
+        policy.optimizer.step()
+        
+        if i_episode % print_every == 0:
+            print('Episode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))
+        
+    return scores
+
+reinforce(
+    policy=policy,
+    n_training_episodes=1000,
+    max_t = 500,
+    gamma = 1,
+    print_every=100
+    )
---
+++ model.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import numpy as np
+import gym
+
+class Model(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super(Model, self).__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+        self.layer1 = nn.Linear(in_dim, 128, dtype=torch.float32)
+        self.layer2 = nn.Linear(128, out_dim, dtype=torch.float32)
+
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim = 1)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=0.001)
+
+    def forward(self, x):
+        x = self.relu(self.layer1(x))
+        x = self.softmax(self.layer2(x))
+
+        return x
+
+        # x = F.relu(self.layer1(x))
+        # x = self.layer2(x)
+        # return F.softmax(x, dim=1)
+    
+    def act(self, state):
+        # logits = self.forward(state.unsqueeze(0))
+        # policy_distribution = torch.distributions.Categorical(logits=logits)
+        # action = logits.argmax()
+        # return action.item(), policy_distribution.log_prob(action)
+
+        probs = self.forward(state.unsqueeze(0))
+        m = torch.distributions.Categorical(probs)
+        action = m.sample()
+        return action.item(), m.log_prob(action)
+
+def test_model():
+    model = Model(4, 2)
+    # test_tensor = torch.tensor([2, 2, 3, 4], dtype = torch.float32)
+    # print(model.forward(test_tensor))
+    env = gym.make("CartPole-v1", render_mode="rgb_array")
+    obs, info = env.reset()
+    test_tensor = obs
+    print(model.act(torch.Tensor([obs])))
+    print(torch.Tensor([obs]).shape)
+    print(torch.Tensor(obs).unsqueeze(0).shape)
+
+if __name__ == "__main__":
+
+    test_model()
