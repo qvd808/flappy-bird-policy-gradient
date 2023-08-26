46f51edbf84eadee5966dfb9d87adbe3ee61bee5
Quang Vinh Dang <dqvinh808@gmail.com>
2023-08-25 18:43:34 -0700

    Adding moveing pipe

--- flappy_bird.py
+++ flappy_bird.py
@@ -13,8 +13,10 @@ pygame.display.set_caption("Flappy Bird")
 
 bg = pygame.image.load("img/bg.png")
 ground_img = pygame.image.load("img/ground.png")
+pipe = pygame.image.load("img/pipe.png")
 
 ground_scroll = 0
+pipe_scroll = SCREEN_WIDTH - pipe.get_width() + 100
 speed = 4
 run = True
 while run:
@@ -28,6 +30,15 @@ while run:
 
     if ground_scroll + ground_img.get_width() == SCREEN_WIDTH:
         ground_scroll = 0
+    
+    ## Have an array of pipe on the screen (depend on the width of the screen). Only check collison for the first pipe since it is more efficient
+    screen.blit(pipe, (pipe_scroll, SCREEN_HEIGHT - pipe.get_height()))
+    pipe_scroll -= 4
+
+    if pipe_scroll + pipe.get_width() == 0:
+        pipe_scroll = SCREEN_WIDTH - pipe.get_width() + 100
+
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
